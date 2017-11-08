0de84a65d185d4a047d4e6f0804a6377720c3d05
sazzad4623 <sazzad111111@gmail.com>
2017-11-08 14:20:34 +0600

    first commit

---
+++ Assets/Scripts/ObstacleScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleScript : MonoBehaviour {
+	
+	//public GameControlScript control;
+	public float objectSpeed = -0.5f;
+	
+	
+	void Update () {
+		if(Time.timeScale==1){
+			transform.Translate(0, objectSpeed, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/PowerupScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupScript : MonoBehaviour {
+	
+	//public GameControlScript control;
+	public float objectSpeed = -0.5f;
+	
+	
+	void Update () {
+		if(Time.timeScale==1){
+			transform.Translate(0, 0, objectSpeed);
+		}
+	}
+}
